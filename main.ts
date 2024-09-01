eea4ba728d432c844232e6de57a1510f91cb31f3
Yoshihiro Nakamura <miggy.ss@gmail.com>
2024-09-01 10:39:27 +0900

    handler

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-Deno.serve(() => new Response("nakamura test"));
+// Deno.serve(() => new Response("nakamura test"));
+const handler = (request: Request, info: Deno.ServeHandlerInfo): Response => {
+    const { method } = request;
+    const remoteHost = info.remoteAddr.hostname;
+    const userAgent = request.headers.get("user-agent");
+
+    return new Response(`hello. nakamura ${new Date()}`);
+};
+
+Deno.serve({ port: 8080, handler: handler });
